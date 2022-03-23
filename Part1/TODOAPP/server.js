b81133da98ac67e5bda7cf9cb9b28000126ffa20
kth0027 <so_dream@naver.com>
2022-03-24 02:52:44 +0900

    중간 점검

--- Part1/TODOAPP/server.js
+++ Part1/TODOAPP/server.js
@@ -12,6 +12,11 @@ app.set('view engine', 'ejs');
 // css
 app.use('/Part1/TODOAPP/public', express.static('public'));
 
+// method override
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+require('dotenv').config()
 
 
 // mongodb+srv://kth0027:<password>@cluster0.lkqof.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
@@ -32,6 +37,16 @@ MongoClient.connect('mongodb+srv://kth0027:xogh0027^^@cluster0.lkqof.mongodb.net
    })
 })
 
+// (env 파일을 적용하는 server.js 코드)
+// var db;
+//   MongoClient.connect(process.env.DB_URL, function(err, client){
+//   if (err) return console.log(err)
+//   db = client.db('Example1');
+//   app.listen(process.env.PORT, function() {
+//     console.log('listening on 8080')
+//   })
+// }) 
+
 
 // app.listen(8080, function () {
 //    console.log('listening on 8080')
@@ -50,6 +65,8 @@ app.get('/write', function (요청, 응답) {
    // 응답.sendFile(__dirname + '/write.html')
    응답.render('write.ejs');
 });
+
+
 
 
 // app.get('/pet', function (요청, 응답) {
@@ -116,3 +133,92 @@ app.get('/detail/:id', function (요청, 응답) {
    })
 
 })
+
+// /edit 
+app.get('/edit/:id', function (요청,응답){
+   // 응답.render('edit.ejs', 파라미터중 :id 번째 게시물 제목/ 날짜)
+   db.collection('post').findOne({_id : parseInt(요청.params.id)}, function(에러, 결과){
+      console.log(결과)
+      응답.render('edit.ejs',{post : 결과})
+   })
+   
+})
+
+app.put('/edit', function(요청, 응답){
+   db.collection('post').updateOne( {_id : parseInt(요청.body.id) }, {$set : { 제목 : 요청.body.title , 날짜 : 요청.body.date }}, function(에러, 결과){
+      console.log('수정완료')
+      응답.redirect('/list')      
+   })
+
+})
+
+// session about passport library
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+// app.use(미들웨어) 미들웨어 : 요청-응답 중간에 뭔가 실행되는 코드
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+// login
+app.get('/login', function(요청,응답){
+   응답.render('login.ejs')
+});
+
+
+app.post('/login', passport.authenticate('local', {
+   failureRedirect : '/fail'
+}), function(요청,응답){
+   응답.redirect('/')
+});
+
+// 마이페이지
+app.get('/mypage', 로그인했니,function(요청,응답){
+   요청.user
+   console.log(요청.user)
+   응답.render('mypage.ejs', {사용자 : 요청.user})
+});
+
+function 로그인했니(요청,응답,next) {
+   // 로그인 후 세션이 있으면 요청.user가 항상 있음
+   if (요청.user) {
+      next()
+   } else {
+      응답.send('로그인 안하셨습니다!')
+   }
+}
+
+passport.use(new LocalStrategy({
+   usernameField: 'id',
+   passwordField: 'pw',
+   session: true,
+   passReqToCallback: false,
+ }, function (입력한아이디, 입력한비번, done) {
+   //console.log(입력한아이디, 입력한비번);
+   db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+     if (에러) return done(에러)
+ 
+     if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+     if (입력한비번 == 결과.pw) {
+       return done(null, 결과)
+     } else {
+       return done(null, false, { message: '비번틀렸어요' })
+     }
+   })
+ }));
+
+// ID를 이용해서 세션을 저장시키는 코드 (로그인 성공시 발동)
+ passport.serializeUser(function(user, done) {
+   done(null, user.id)
+ });
+
+ // 이세션 데잍를 가진 사람을 DB에서 찾아주세요 (마이페이지 접속시)
+ passport.deserializeUser(function(아이디, done){
+    db.collection('login').findOne({id:아이디}, function(에러,결과){
+      done(null, 결과)
+    })
+  
+ });
+
