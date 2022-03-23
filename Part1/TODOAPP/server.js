b8724ac92a0a3a04075293602438ed5c4160ea50
kth0027 <so_dream@naver.com>
2022-03-23 17:02:46 +0900

    part2 종료

--- Part1/TODOAPP/server.js
+++ Part1/TODOAPP/server.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require('mongodb').MongoClient;
 // ejs
 app.set('view engine', 'ejs');
+// css
+app.use('/Part1/TODOAPP/public', express.static('public'));
 
 
 
@@ -40,20 +42,24 @@ MongoClient.connect('mongodb+srv://kth0027:xogh0027^^@cluster0.lkqof.mongodb.net
 
 
 app.get('/', function (요청, 응답) {
-   응답.sendFile(__dirname + '/index.html')
-});
-
-app.get('/pet', function (요청, 응답) {
-   응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
-});
-
-app.get('/beauty', function (요청, 응답) {
-   응답.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
+   // 응답.sendFile(__dirname + '/index.html')
+   응답.render('index.ejs');
 });
 
 app.get('/write', function (요청, 응답) {
-   응답.sendFile(__dirname + '/write.html')
+   // 응답.sendFile(__dirname + '/write.html')
+   응답.render('write.ejs');
 });
+
+
+// app.get('/pet', function (요청, 응답) {
+//    응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
+// });
+
+// app.get('/beauty', function (요청, 응답) {
+//    응답.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
+// });
+
 
 
 
@@ -62,32 +68,51 @@ app.get('/write', function (요청, 응답) {
 // #1
 // 어떤 사람이 /add 경로로 post 요청을 하면...
 // ?? 를 해주세요
-app.post('/add', function(요청, 응답){
+app.post('/add', function (요청, 응답) {
    응답.send('전송완료');
-   db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
+   db.collection('counter').findOne({ name: '게시물갯수' }, function (에러, 결과) {
       console.log(결과.totalPost)
-     var 총게시물갯수 = 결과.totalPost;
-     
-     db.collection('post').insertOne( { _id : 총게시물갯수 + 1, 제목 : 요청.body.title, 날짜 : 요청.body.date } , function(){
-       console.log('저장완료');
-      // counter라는 콜렉션에 있는 totalPost 라는 항목도 1 증가시켜야 함 (수정)
+      var 총게시물갯수 = 결과.totalPost;
+
+      db.collection('post').insertOne({ _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date }, function () {
+         console.log('저장완료');
+         // counter라는 콜렉션에 있는 totalPost 라는 항목도 1 증가시켜야 함 (수정)
          // db.collection('counter').updateOne({수정할 데이터},{수정값},function(){})
-         db.collection('counter').updateOne({name:'게시물갯수'},{$inc : {totalPost :1} },function(에러, 결과){
-            if(에러){return console.log(에러) }
+         db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
+            if (에러) { return console.log(에러) }
          })
-     });
+      });
    });
-   
- });
+
+});
 
 
 // #2
 // /list 로  get요청으로 접속하면
 // 실제DB에 저장된 데이터들로 예쁘게 꾸며진 HTML을 보여줌
-app.get('/list', function(요청, 응답){
-   db.collection('post').find().toArray(function(에러, 결과){
-     console.log(결과)
-     응답.render('list.ejs', { posts : 결과 })
+app.get('/list', function (요청, 응답) {
+   db.collection('post').find().toArray(function (에러, 결과) {
+      console.log(결과);
+      응답.render('list.ejs', { posts: 결과 });
    })
- })
+})
 
+// 삭제
+app.delete('/delete', function (요청, 응답) {
+   console.log(요청.body);
+   요청.body._id = parseInt(요청.body._id);
+   // 요청.body 에 담겨온 게시물번호를 가진 글을 db에서 찾아서 삭제해주세요
+   db.collection('post').deleteOne(요청.body, function (에러, 결과) {
+      console.log('삭제완료');
+      응답.status(200).send({ message: '성공했습니다' });
+   })
+})
+
+// /detail 로 접속하면 detail.ejs 보여줌
+app.get('/detail/:id', function (요청, 응답) {
+   db.collection('post').findOne({ _id: parseInt(요청.params.id) }, function (에러, 결과) {
+      console.log(결과);
+      응답.render('detail.ejs', { data : 결과 });
+   })
+
+})
